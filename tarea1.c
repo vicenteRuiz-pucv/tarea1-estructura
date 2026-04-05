7161e691cf27b306e1eb44ce91c8c20c7ffc3ea9
VICENTE IGNACIO RUIZ SORIANO <56021841-vicenteRuizSor@users.noreply.replit.com>
2026-04-05 05:53:35 +0000

    Termino de avance

--- tarea1.c
+++ tarea1.c
@@ -7,8 +7,90 @@
 PASO A PASO PARA ESTE PROGRAMA
 DEBO HACER 3 FUNCIONALIDADES.. NUEVA CATEGORIA, ELIMINAR CATEGORIA Y MOSTRAR CATEGORIA.
 DEBO DEFINIR Y DECLARAR BIEN LOS STRUCTS QUE UTILIZARE, Y SIMPLEMENTE DEBO USAR LOS TDAS, MAS NO IMPLEMENTARLOS
-
 */
+//NECESITO VER COMO IMPLEMENTAR LA HORA..
+typedef struct
+{
+    char* descripcion;
+    char* categoria;
+    char* hora;
+
+}tipoTarea;
+
+typedef struct
+{
+    //usare cadenas dinamicas!
+    char* nombreCat;
+    //lista que va a contener structs de tipo tarea
+    List* tareas;
+}tipoCategoria;
+
+//FUNCION LLENAR LISTA O POBLARLA
+
+void llenarLista(List* categorias)
+{
+  /*
+  que es lo que queremos hacer?
+  debemos registrar una categoria, por lo que necesitamos un bucle? 
+  o algo para poder llenar la lista de categorias ya inicializada
+  QUE NECESITAMOS?
+  LISTA DE CATEGORIAS-> CONTIENE DATOS TIPOCATEGORIA -> CONTIENEN LISTA DE TAREAS EN CADA POS!
+  LA LISTA DE CATEGORIAS YA SE INICIALIZO!
+  FALTA POBLAR ESA LISTA CON LA NUEVA CATEGORIA!
+  EL PASO A PASO SERA.
+  
+  DECLARAR EL STRUCT TIPO CATEGORIA, LEER Y GUARDAR EN SU CAMPO EL NOMBRE INGRESADO!
+  USAR OPCIONES DE FORMATO? 
+
+  y debo pasar ese struct a la lista de categorias! con un casting?
+
+  el nombre de cada categoria seria el campo con el string!
+  */
+  //declaraciones y reservas de memoria para el struct y su nombre
+  tipoCategoria* nuevaCategoria = (tipoCategoria *)malloc(sizeof(tipoCategoria));
+  nuevaCategoria->nombreCat= (char*) malloc(16 * sizeof(char));
+  //formato
+  printf("Ingrese un nombre para su categoria: ");
+  //guardamos en el struct
+  scanf("%15s", nuevaCategoria->nombreCat);
+  //prueba de lo guardado
+  printf("nombre guardado: %s\n",nuevaCategoria->nombreCat);
+  printf("Su Registro se ha guardado exitosamente!\n");
+  list_pushBack(categorias,nuevaCategoria);
+}
+//funcion eliminar categoria
+/*
+en resumen, lo que debe hacer es a partir de una coincidencia de categoria
+recorrer la lista de categorias y en cada pos preguntar si la coincidencia
+es igual, si lo es, eliminar el nodo completo! con pop
+*/
+void eliminarCat(List* categorias)
+{
+  //PEDIR EL STRING CORRESPONDIENTE
+  char *buscado = (char *)malloc(16 * sizeof(char));
+  scanf("%15s",buscado);
+ //EN UN BUCLE WHILE INDEXAR Y BUSCAR LA COINCIDENCIA
+  tipoCategoria* actual= (tipoCategoria *) list_first(categorias);
+  while(1)
+  {
+        if(strcmp(actual->nombreCat,buscado) == 0)
+        {
+          list_popCurrent(categorias);
+          printf("se ha eliminado la categoria %s\n",buscado);
+          return;
+        }
+        actual= (tipoCategoria *) list_next(categorias);
+  }
+}
+
+
+
+
+
+
+
+
+
 // Menú principal
 void mostrarMenuPrincipal() {
   limpiarPantalla();
@@ -29,16 +111,39 @@ void mostrarMenuPrincipal() {
 void registrar_categorias(List *categorias) {
   printf("Registrar nueva categoría\n");
   // Aquí implementarías la lógica para registrar una nueva categoría
+  llenarLista(categorias);
+}
+
+void eliminar_categorias(List* categorias){
+  printf("Eliminar una categoria\n");
+  if(list_first(categorias) == NULL) printf("La lista esta vacía, seleccione otra opcion\n");
+  else
+  {
+      eliminarCat(categorias);
+  }
+  
 }
 
 void mostrar_categorias(List *categorias) {
   // Mostrar categorías
   printf("Categorías:\n");
   // Aquí implementarías la lógica para mostrar las categorías
+  if(list_first(categorias) == NULL) printf("La lista esta vacía, seleccione otra opcion\n");
+  else
+  {
+  tipoCategoria* actual= (tipoCategoria *) list_first(categorias);
+  
+  while(actual != NULL)
+  {
+        printf("%s\n",actual->nombreCat);    
+        actual= (tipoCategoria *) list_next(categorias);
+  }
+  } 
 }
 
 int main() {
   char opcion;
+  //LISTA DE CATEGORIAS YA INICIALIZADA
   List *categorias = list_create(); // Lista para almacenar categorías
 
   do {
@@ -53,6 +158,7 @@ int main() {
       break;
     case '2':
       // Lógica para eliminar una categoría
+      eliminar_categorias(categorias);
       break;
     case '3':
       mostrar_categorias(categorias);
@@ -70,7 +176,7 @@ int main() {
       // Lógica para filtrar por categoría
       break;
     case '8':
-      puts("Saliendo del sistema de gestión hospitalaria...");
+      puts("Saliendo del sistema de gestión de tareas...");
       break;
     default:
       puts("Opción no válida. Por favor, intente de nuevo.");
